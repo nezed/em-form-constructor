79b6b100169d0d448a71ecc3615b6d076c8b45f4
NeZeD [Mac Air] <nezed@ya.ru>
2016-06-21 18:14:27 +0300

    This is GH-PAGES!

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,9 +1,10 @@
 git checkout gh-pages
 git merge master --ff
 npm run build
+git add "build/*"
 
 LAST_CI=`git rev-list master | head -n 1`
-git commit -am 'Build gh-pages from #${LAST_CI:0:5}'
+git commit -am "Build gh-pages from #${LAST_CI:0:5}"
 
 git push origin gh-pages
 git checkout @{-1}
